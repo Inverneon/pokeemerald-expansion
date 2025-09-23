fc4d1a20034d414c49634f58ba31fa4f04637299
Inverneon <inverneon2390@gmail.com>
2025-09-22 22:05:00 -0700

    map additions

--- data/event_scripts.s
+++ data/event_scripts.s
@@ -1157,3 +1157,9 @@ EventScript_VsSeekerChargingDone::
 	.include "data/text/save.inc"
 	.include "data/text/birch_speech.inc"
 	.include "data/scripts/dexnav.inc"
+
+	.include "data/maps/Route_131/scripts.inc"
+
+	.include "data/maps/Route_135/scripts.inc"
+
+	.include "data/maps/Ryado_Labyrinth/scripts.inc"
